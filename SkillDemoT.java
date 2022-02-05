209cded75bccc46abdabe13b6bb96e7ac6a09010
vickyc0221 <vcaldero@ucsd.edu>
2022-02-04 16:45:11 -0800

    demo

--- SkillDemoT.java
+++ SkillDemoT.java
@@ -1,2 +1,5 @@
 public class SkillDemoT{
+    public static int add(int n, int m){
+		return n+m;
+	}
 }
---
+++ SkillDemoTTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTTest{
+
+	@Test
+	public void addition() {
+		assertEquals(2, SkillDemoT.add(1, 3));
+	}
+}
